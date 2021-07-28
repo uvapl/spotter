f2e65f10e9a2886757c9024ea44cc08b9ea1e453
Martijn Stegeman <martijn@stgm.nl>
2021-07-28 11:35:02 +0200

    seeds explanation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,37 @@ this_year = Date.current.year
 next_week = (Date.current + 7).cweek
 next_year = (Date.current + 7).year # this is not really next year, but next week's year
 
+# ruby weekdays start at Monday = 1
 slots = {
-    1 => {
+    1 => {        # Monday
         9 => 1,
         10 => 1,
         11 => 2,
         12 => 2,
         13 => 1
     },
-    2 => {
+    2 => {        # Tuesday
         9 => 1,
         10 => 1,
         11 => 2,
         12 => 2,
         13 => 1
     },
-    3 => {
+    3 => {        # Wednesday
         9 => 1,
         10 => 1,
         11 => 2,
         12 => 2,
         13 => 1
     },
-    4 => {
+    4 => {        # Thursday
         9 => 1,
         10 => 1,
         11 => 2,
         12 => 2,
         13 => 1
     },
-    5 => {
+    5 => {        # Friday
         12 => 2,
         13 => 1
     },
