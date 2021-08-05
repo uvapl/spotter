70ff5972bea7ffbd5cdd4b91601bc2de57b3e8fa
Martijn Stegeman <martijn@stgm.nl>
2021-08-05 17:11:45 +0200

    prepare i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     include AuthenticationHelper
     before_action do
+        I18n.locale = :nl
         head :unauthorized unless request.session['cas'].present?
     end
 end
