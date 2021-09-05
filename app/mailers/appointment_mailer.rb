8d7c63b56ac49378ee52798c9b59d03c28ffe9b7
Martijn Stegeman <martijn@stgm.nl>
2021-09-05 21:51:27 +0200

    correct to: mail adress

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -2,6 +2,6 @@ class AppointmentMailer < ApplicationMailer
     def confirmation_mail
         @appointment = params[:appointment]
         @user = @appointment.user
-        mail to: @user, subject: 'Bevestiging afspraak programmeren'
+        mail to: @user.email, subject: 'Bevestiging afspraak programmeren'
     end
 end
