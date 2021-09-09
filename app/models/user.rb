23013657b554a0a028d031603f4e2b75e2edb1c4
Björn Out <38955258+bjorn-out@users.noreply.github.com>
2021-09-09 16:47:28 +0200

    allow for names with diacritics (#8)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :appointments
 
-    validates :name, format: { with: /\A\w{2,} [\w\s]{2,}\z/i, on: :create }
+    validates :name, format: { with: /\A[[:alpha:]]{2,} [[:alpha:]\s]{2,}\z/i, on: :create }
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
